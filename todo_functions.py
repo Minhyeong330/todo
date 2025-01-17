8c6c4855fb8b0a7e1d775a9a9102bd744b92c834
dulusian <wchoi1994@gmail.com>
2025-01-17 17:53:41 -0600

    added the type to function

--- todo_functions.py
+++ todo_functions.py
@@ -1,6 +1,6 @@
 import json
 
-def file_open(file_path):
+def file_open(file_path: str) -> dict:
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
         return data
