3350e6d9a0e2b2c79c332c272a2be86bfe7929eb
minhyeong <minhyeong9986@gmail.com>
2025-01-20 15:21:00 -0600

    nothing

--- todo_functions.py
+++ todo_functions.py
@@ -1,11 +1,18 @@
 import json
 
 def file_open(file_path: str) -> dict:
+    """Open a json file
+    The type of file_path is 'str'
+    The return type will be 'dict'
+    """
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
         return data
       
 def user_exists(file_path: str, user_name: str) -> dict:
+    """To find if there is a username as the values in the key
+    The key is "names"
+    """
     file_open(file_path)
     data = file_open(file_path)
     if user_name in data["names"]:
@@ -13,7 +20,9 @@ def user_exists(file_path: str, user_name: str) -> dict:
     elif user_name not in data["names"]:
         return False
 
-def user_add(file_path: str, new_user: str) -> dict:
+def add_user(file_path: str, new_user: str) -> dict:
+    """Add new_user to "names" as a dictionary
+    """
     file_open(file_path)
     data = file_open(file_path)
     data["names"].update({new_user: []})
@@ -21,11 +30,16 @@ def user_add(file_path: str, new_user: str) -> dict:
         json.dump(data, file, indent=4)
 
 def view_user_list(file_path: str) -> dict:
+    """View userlist. This is the values of username (key)
+    """
     file_open(file_path)
     data = file_open(file_path)
     print(data['names'])
 
-def remove_user(file_path: str, list_number: str) -> dict:
+def remove_user(file_path: str, list_number: int) -> dict:
+    """Remove user from the username
+    List_number = index number of the username
+    """
     file_open(file_path)
     data = file_open(file_path)
     data['names'].pop(list_number)
@@ -33,11 +47,15 @@ def remove_user(file_path: str, list_number: str) -> dict:
         json.dump(data, file, indent=4)
 
 def view_to_do_list(file_path: str, user_name: str) -> list:
+    """Show the to-do list according to 'user_name'
+    """
     file_open(file_path)
     data = file_open(file_path)
     print(data["names"][user_name])
 
 def add_to_do(file_path: str, user_name: str, new_to_do: str) -> list:
+    """Add new_to_do to the to-do list
+    """
     file_open(file_path)
     data = file_open(file_path)
     data["names"][user_name].append(new_to_do)
@@ -45,8 +63,11 @@ def add_to_do(file_path: str, user_name: str, new_to_do: str) -> list:
         json.dump(data, file, indent=4)
 
 def remove_to_do(file_path: str, user_name: str, list_number: int) -> list:
+    """Remove to-do from the lis. List_number is the index number.
+    The index number starts at 0, so this function adjusts the start point to '1'
+    """
     file_open(file_path)
     data = file_open(file_path)
-    data["name"][user_name].pop(list_number)
+    data["names"][user_name].pop(list_number-1)
     with open(file_path, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4)
